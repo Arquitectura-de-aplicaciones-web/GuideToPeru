b8b60351acea14e175d999eb99ceaaa393029dcb
EdeerJ <u20201b780@upc.edu.pe>
2023-04-25 23:21:17 -0500

    Primera version del menu
    Estructura del menu compuesto de un sidenav y eliminacion del anterior menu

--- src/app/component/cliente/cliente-listar/cliente-listar.component.ts
+++ src/app/component/cliente/cliente-listar/cliente-listar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
+
 import { Cliente } from 'src/app/model/clientes';
 import { ClienteService } from 'src/app/service/cliente.service';
 import { MatTableDataSource } from '@angular/material/table'
@@ -11,6 +13,14 @@ import { ClienteDialogoComponent } from './cliente-dialogo/cliente-dialogo.compo
   styleUrls: ['./cliente-listar.component.css']
 })
 export class ClienteListarComponent implements OnInit {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  reason = '';
+  close(reason: string) {
+    this.reason = reason;
+    this.sidenav.close();
+  }
+  shouldRun = true;
   dataSource: MatTableDataSource<Cliente> = new MatTableDataSource();
   lista: Cliente[] = []
   idMayor: number = 0
--- src/app/component/cliente/menu/menu.component.ts
+++ src/app/component/cliente/menu/menu.component.ts
@@ -1,45 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import {FlatTreeControl} from '@angular/cdk/tree';
-import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
-import { Router } from '@angular/router';
-import { MatTreeNestedDataSource } from '@angular/material/tree';
-import { NestedTreeControl } from '@angular/cdk/tree';
-
-interface Node {
-  name: string;
-  children?: Node[];
-}
-
-const TREE_DATA: Node[] = [
-  {
-    name: 'Clientes',
-    children: [
-      { name: 'Registros' },
-      { name: 'Insertar' }
-    ]
-  }
-];
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
 
-  treeControl = new NestedTreeControl<Node>(node => node.children);
-  dataSource = new MatTreeNestedDataSource<Node>();
-  constructor(private router: Router) {
-    this.dataSource.data = TREE_DATA;
+  reason = '';
+  close(reason: string) {
+    this.reason = reason;
+    this.sidenav.close();
   }
+  shouldRun = true;
+  //shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(window.location.host);
+  ngOnInit(): void {  
 
-  hasChild = (_: number, node: Node) => !!node.children && node.children.length > 0;
-
-onNodeClick(node: Node) {
-  if (node.children) {
-    this.treeControl.toggle(node);
-  } else {
-    this.router.navigate(['/route-to-' + node.name.toLowerCase()]);
   }
-}
 }
